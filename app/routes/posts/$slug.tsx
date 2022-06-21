4bff45f7b12ff9c90c9af4e6ee845c298c60c4d1
mikias.wk <mikias.wk@2f-capital.com>
2022-06-21 11:45:57 +0300

    get typescript coverage with invariant

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -2,27 +2,30 @@ import { useLoaderData } from "@remix-run/react"
 import { json, LoaderFunction } from "@remix-run/server-runtime"
 import { getPost } from "~/models/post.server"
 import {marked} from 'marked'
+import invariant from "tiny-invariant"
 
 type LoaderData ={
-    post: Awaited<ReturnType<typeof getPost>>,
+    title: string,
     html: string
 }
 
 export const loader: LoaderFunction = async({params})=>{
     const {slug} = params
 
+    invariant(slug, 'slug is required')
     const post = await getPost(slug)
 
+    invariant(post, `Post not found: ${slug}`)
     const html = marked(post.markdown)
 
-    return json<LoaderData>({post, html})
+    return json<LoaderData>({title: post.title, html})
 }
 
 const PostRoute = () => {
-    const {post, html} = useLoaderData() as LoaderData
+    const {title, html} = useLoaderData() as LoaderData
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">{post?.title}</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
       <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   )
