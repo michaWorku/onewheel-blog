e9ff75c7374cc2ad918899697e0e7f2b913b325e
mikias.wk <mikias.wk@2f-capital.com>
2022-06-21 10:45:29 +0300

    fetching post using loader done

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,8 +1,41 @@
+import { Link, useLoaderData } from "@remix-run/react"
+import { json, LoaderFunction } from "@remix-run/server-runtime"
+
+
+export const loader: LoaderFunction =()=>{
+    const posts = [
+        {
+            slug: 'my-first-post',
+            title: 'My First Post'
+        },
+        {
+            slug: 'trail-riding-with-onewheel',
+            title: 'Trail Riding With Onewheel'
+        }
+    ]
+
+    return json({posts})
+}
 
 const PostRoutes = () => {
+    const {posts} = useLoaderData()
   return (
     <main>
         <h1>Posts</h1>
+        <ul>
+            {
+                posts.map((post)=>(
+                    <li key= {post.slug}>
+                        <Link 
+                            to={post.slug}
+                            className='text-blue-600 underline'
+                        >
+                            {post.title}
+                        </Link>
+                    </li>
+                ))
+            }
+        </ul>
     </main>
   )
 }
